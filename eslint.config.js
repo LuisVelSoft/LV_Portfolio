03ff95adbef211839576b6fe00a24b8bdc1eb59e
LuisVelSoft <luisvel@outlook.com>
2025-12-31 10:04:03 -0500

    chore: added eslint extension and config file

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,8 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      "indent": ["error", 2],
+      "extends": ["eslint:recommended", "plugin:prettier/recommended"],
     },
   },
 ])
